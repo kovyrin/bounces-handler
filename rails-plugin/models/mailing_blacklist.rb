2566bb2f004614f5b343f98874d371d4e048c0d8
Alexey Kovyrin <alexey@kovyrin.net>
2008-07-14 18:07:28 -0400

    started implementing models

--- rails-plugin/models/mailing_blacklist.rb
+++ rails-plugin/models/mailing_blacklist.rb
@@ -1,4 +1,27 @@
 class Blacklist < ActiveRecord::Base
   establish_connection configurations[RAILS_ENV]['bounces_handler']
   set_table_name 'mailing_blacklist'
+  
+  def self.banned?(email, level = nil)
+    parsed_email = parse_email(email)
+    
+    domain = MailingDomain.find_by_name_crc(parsed_email[:domain])
+    return false unless domain
+    
+    conditions = {}
+    conditions[:domain_id] = domain.id
+    conditions[:user] = parsed_email[:user]
+    conditions[:user_crc32] = Zlib.crc32(parsed_email[:user])
+    count(conditions) > 0
+  end
+  
+  def self.hard_banned?(email)
+    banned?(email, :hard)
+  end
+
+  def self.soft_banned?(email)
+    banned?(email, :soft)
+  end
+  
+  
 end
--- rails-plugin/models/mailing_domain.rb
+++ rails-plugin/models/mailing_domain.rb
@@ -1,4 +1,15 @@
+require 'zlib'
+
 class MailingDomain < ActiveRecord::Base
   establish_connection configurations[RAILS_ENV]['bounces_handler']
   belongs_to :mailing_blacklist
+  before_validation :fill_name_crc32_field
+
+  def fill_name_crc32_field
+    self['name_crc32'] = Zlib.crc32(self['name'])
+  end
+  
+  def self.find_by_name_crc(name)
+    find(:first, :conditions => { :name => name, :name_crc32 => Zlib.crc32(name)})
+  end
 end
