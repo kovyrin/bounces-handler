02c44480c685b86b35bf43b2725ae5db74d8eebf
Alexey Kovyrin <alexey@kovyrin.net>
2008-08-04 22:13:15 -0400

    Simple feedback loops processor added

--- email-processor/bounce-processor.pl
+++ email-processor/bounce-processor.pl
@@ -1,6 +1,11 @@
 #!/usr/bin/env perl
 
 use strict;
+use Cwd;
+use File::Basename;
+
+our $SELF_DIR = dirname(Cwd::abs_path(__FILE__));
+require $SELF_DIR . '/lib/bounce_db.pm';
 
 use DBI;
 use Mail::DeliveryStatus::BounceParser;
@@ -46,53 +51,3 @@ for my $report ($bounce->reports) {
 
 exit(0);
 
-#-------------------------------------------------------------------------------
-sub RegisterBounce($$$) {
-    my ($email, $reason, $dbh) = @_;
-    print "BOUNCE: $email with reason $reason\n";
-    
-    my ($email_user, $email_domain) = split(/\@/, lc($email));
-    unless ($email_user && $email_domain) {
-        print "Invalid email address!\n";
-        exit(1);
-    }
-    my $domain_id = RegisterBounceDomain($email_domain, $dbh);
-    
-    my $level = ($reason eq 'over_quota') ? 'soft' : 'hard';
-    RegisterBounceEmail($email_user, $domain_id, $reason, $level, $dbh);
-}
-
-#-------------------------------------------------------------------------------
-sub RegisterBounceDomain($$) {
-    my ($domain, $dbh) = @_;
-    
-    # Lookup domain name
-    my $sth = $dbh->prepare("SELECT id FROM $domains_table WHERE name = ? AND name_crc32 = CRC32(?)");
-    $sth->execute($domain, $domain);
-    my $row = $sth->fetchrow_hashref;
-    return $row->{id} if $row;
-
-    # If not found, create it
-    print "Registering domain: $domain\n";
-    $sth = $dbh->prepare("INSERT INTO $domains_table SET name = ?, name_crc32 = CRC32(?)");
-    $sth->execute($domain, $domain);
-    return $dbh->{'mysql_insertid'};
-}
-
-#-------------------------------------------------------------------------------
-sub RegisterBounceEmail($$$$$) {
-    my ($email_user, $domain_id, $reason, $level, $dbh) = @_;
-    
-    my $sql = "
-        INSERT INTO $blacklist_table SET
-          domain_id = ?,
-          user_crc32 = CRC32(?),
-          user = ?,
-          source = 'bounce',
-          level = ?,
-          reason = ?,
-          created_at = NOW()
-    ";
-    my $sth = $dbh->prepare($sql);
-    $sth->execute($domain_id, $email_user, $email_user, $level, $reason);
-}
---
+++ email-processor/feedback-loop-processor.pl
@@ -0,0 +1,109 @@
+#!/usr/bin/env perl
+
+use strict;
+use Cwd;
+use File::Basename;
+
+our $SELF_DIR = dirname(Cwd::abs_path(__FILE__));
+require $SELF_DIR . '/lib/bounce_db.pm';
+
+use DBI;
+use Email::ARF::Report;
+use Data::Dumper;
+
+#-------------------------------------------------------------------------------
+
+our $mysql_host = 'localhost';
+our $mysql_user = 'root';
+our $mysql_pass = '';
+our $mysql_db = 'bounces';
+
+our $blacklist_table = "mailing_blacklist";
+our $domains_table = "mailing_domains";
+
+#-------------------------------------------------------------------------------
+# Read the report from stdin
+my $report_text = '';
+while(<>) {
+    $report_text .= $_;
+}
+
+# Try to parse the report
+my $report = eval {
+    Email::MIME->new($report_text);
+};
+
+if ($report) {
+    # Process the report
+    my $report_email = ProcessReport($report);
+    HandleFeedback($report_email) if ($report_email);
+    
+    $report_email = ProcessUnknownReport($report);
+    HandleFeedback($report_email) if ($report_email);
+}
+
+#FIXME: forward the message to a human for review
+
+exit(0);
+
+#-------------------------------------------------------------------------------
+sub ProcessReport($) {
+    my $report = shift;
+
+    my $from = $report->header('From');
+    
+    return ProcessHotmailReport($report) if ($from eq 'staff@hotmail.com');
+    return ProcessUnknownReport($report) if ($from eq 'scomp@aol.net');
+
+    print "Warning: Unknown report source!\n";
+    return undef;
+}
+
+#-------------------------------------------------------------------------------
+sub ProcessHotmailReport($) {
+    my $report = shift;
+    
+    my @parts = $report->parts;
+    for my $part (@parts) {
+        my $part_email = eval { Email::MIME->new($part->body_raw); };
+        next unless ($part_email);
+        return $part_email->header('To');
+    }
+    
+    print "ERROR: Can't parse hotmail report!\n";
+    return undef;
+}
+
+#-------------------------------------------------------------------------------
+sub ProcessUnknownReport($) {
+    my $report = shift;
+
+    my $from = $report->header('From');
+    my $to = $report->header('To');
+
+    # Here you can do your own processing stuff
+    # The code below is for scribd.com
+    
+    my $body = $report->body_raw;
+    if ($body =~ /http:\/\/www.scribd.com\/optout\/\w+\/(.*\@.*)/) {
+        return $1;
+    }
+    
+    print "ERROR: Can't parse unknown report from '$from' to '$to')!\n";
+    return undef;
+}
+
+#-------------------------------------------------------------------------------
+sub HandleFeedback($) {
+    my $email = shift;
+    
+    print("Blacklisting email: $email\n");
+    
+    # Connect to mysql to save blacklist information
+    my $dbh = DBI->connect("DBI:mysql:database=$mysql_db;host=$mysql_host", $mysql_user, $mysql_pass, {'RaiseError' => 1});
+    
+    # Register the record
+    RegisterBounce($email, 'feedback-loop', $dbh);
+    
+    exit(0)
+}
